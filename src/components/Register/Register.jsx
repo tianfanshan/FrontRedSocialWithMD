18284c815e83f98bf007d6b0dc70fdee3a8ddc2c
laopengyou <stf30111994@gmail.com>
2022-06-29 21:21:02 +0200

    29062022

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const Register = () => {
+    const [formData,setFormData] = useState({
+        
+    })
+  return (
+    <div>Register</div>
+  )
+}
+
+export default Register
