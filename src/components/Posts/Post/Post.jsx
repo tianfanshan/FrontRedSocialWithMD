6e755da89ec1d45010f6102f81f5f79974f002d5
laopengyou <stf30111994@gmail.com>
2022-06-30 15:26:44 +0200

    get all post finished

--- src/components/Posts/Post/Post.jsx
+++ src/components/Posts/Post/Post.jsx
@@ -1,15 +1,15 @@
-import { useDispatch,useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 
 const Post = () => {
-  const {posts} = useSelector((state)=>state.posts)
+  const { posts } = useSelector((state) => state.posts)
   console.log(posts)
   // const dispatch = useDispatch()
 
-  const post=posts.map(pos=>{
-    return(
+  const post = posts.map(pos => {
+    console.log(pos)
+    return (
       <div key={pos._id}>
-        <p>{pos.name}</p>
-
+        <p>{pos.body}</p>
       </div>
     )
   })
--- src/features/posts/postsService.js
+++ src/features/posts/postsService.js
@@ -4,7 +4,7 @@ const API_URL = "http://localhost:8000"
 
 const getAllPost = async () => {
     const res = await axios.get(API_URL + '/posts')
-    console.log(res)
+    return res.data
 }
 
 const postsService = {
