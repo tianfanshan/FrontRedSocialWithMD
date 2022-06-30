242974ac6ec8a98ced9270b1a4919aa27e172526
tianfanshan <stf30111994@gmail.com>
2022-06-30 12:58:28 +0200

    components users done

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,40 +1,40 @@
-import axios from 'axios'
+import axios from "axios";
 
-const API_URL = 'http://localhost:8000'
+const API_URL = "http://localhost:8000";
 
 const register = async (userData) => {
-    console.log(userData)
-    const res = await axios.post(API_URL + '/users', userData);
-    console.log(res.data)
-    return res.data
-}
+  console.log(userData);
+  const res = await axios.post(API_URL + "/users", userData);
+  console.log(res.data);
+  return res.data;
+};
 
 const login = async (user) => {
-    console.log(user)
-    const res = await axios.post(API_URL + '/users/login',user)
-    console.log(res)
-    if(res.data){
-        localStorage.setItem('user',JSON.stringify(res.data))
-    }
-    return res.data
-}
+  console.log(user);
+  const res = await axios.post(API_URL + "/users/login", user);
+  console.log(res);
+  if (res.data) {
+    localStorage.setItem("user", JSON.stringify(res.data));
+  }
+  return res.data;
+};
 
 const logout = async () => {
-    const user = JSON.parse(localStorage.getItem('user'))
-    const res = await axios.put(API_URL + '/users/logout',{
-        headers:{
-            authorization : user?.token
-        }
-    })
-    if(res.data){
-        localStorage.removeItem('user')
-    }
-}
+  const user = JSON.parse(localStorage.getItem("user"));
+  const res = await axios.put(API_URL + "/users/logout", {
+    headers: {
+      authorization: user?.token,
+    },
+  });
+  if (res.data) {
+    localStorage.removeItem("user");
+  }
+};
 
 const authService = {
-    register,
-    login,
-    logout
-}
+  register,
+  login,
+  logout,
+};
 
-export default authService
+export default authService;
