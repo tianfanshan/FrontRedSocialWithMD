e3b6a6c44e5fc71795c6d949102fa1f0685f6925
laopengyou <stf30111994@gmail.com>
2022-06-30 07:25:20 +0200

    component register work

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -2,10 +2,28 @@ import React, { useState } from 'react'
 
 const Register = () => {
     const [formData,setFormData] = useState({
-        
+        name:'',
+        email:'',
+        password:''
     })
+    const {name,email,password} = formData
+    const onChange = (e) => {
+      setFormData((prevState)=>({
+        ...prevState,
+        [e.target.name]:e.target.value
+      }))
+    }
+    const onSubmit = (e) => {
+      e.preventDefault()
+      console.log('formData'.formData)
+    } 
   return (
-    <div>Register</div>
+    <form onSubmit={onSubmit}>
+      <input type="text" name='name' value={name} onChange={onChange}/>
+      <input type="email" name='email' value={email} onChange={onChange}/>
+      <input type="password" name='password' value={password} onChange={onChange}/>
+      <button type='submit'>Register</button>
+    </form>
   )
 }
 
