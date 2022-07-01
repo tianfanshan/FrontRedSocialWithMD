5394e1ed435e4c59e458868c2ac4664a7adfd861
laopengyou <stf30111994@gmail.com>
2022-07-01 17:32:09 +0200

    ant-d regist form

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { register } from '../../features/auth/authSlice'
-import { notification } from 'antd'
 import { useNavigate } from 'react-router'
+import { Button, Form, Input, notification,InputNumber } from 'antd';
 
 
 const Register = () => {
@@ -29,8 +29,27 @@ const Register = () => {
       [e.target.name]: e.target.value
     }))
   }
-  const onSubmit = (e) => {
-    e.preventDefault()
+  // const onSubmit = (e) => {
+  //   e.preventDefault()
+  //   if (password !== password2) {
+  //     return notification.error({
+  //       message: 'Error',
+  //       description: 'Passwords do not match'
+  //     })
+  //   } else {
+  //     dispatch(register(formData))
+  //     console.log(formData)
+  //     notification.success({
+  //       message: message,
+  //       description: 'Happy hacking!'
+  //     })
+  //     setTimeout(() => {
+  //       navigate('/login')
+  //     }, 3000)
+  //   }
+  // }
+
+  const onFinish = () => {
     if (password !== password2) {
       return notification.error({
         message: 'Error',
@@ -47,16 +66,119 @@ const Register = () => {
         navigate('/login')
       }, 3000)
     }
-  }
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <form onSubmit={onSubmit}>
-      <input type="text" name='name' value={name} onChange={onChange} />
-      <input type="email" name='email' value={email} onChange={onChange} />
-      <input type="number" name='age' value={age} onChange={onChange} />
-      <input type="password" name='password' value={password} onChange={onChange} />
-      <input type="password" name='password2' value={password2} onChange={onChange} />
-      <button type='submit'>Register</button>
-    </form>
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Username"
+        name="name"
+        value={name} 
+        onChange={onChange}
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="email"
+        label="email"
+        rules={[
+          {
+            type: 'email',
+            message: 'The input is not valid E-mail!',
+          },
+          {
+            required: true,
+            message: 'Please input your E-mail!',
+          },
+        ]}
+        value={email} 
+        onChange={onChange}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item label="InputNumber" 
+      name='age'
+      value={age} 
+      onChange={onChange}
+      >
+        <InputNumber />
+        </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        value={password} 
+        onChange={onChange}
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password2"
+        value={password2} 
+        onChange={onChange}
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="submit" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+    // <form onSubmit={onSubmit}>
+    //   <input type="text" name='name' value={name} onChange={onChange} />
+    //   <input type="email" name='email' value={email} onChange={onChange} />
+    //   <input type="number" name='age' value={age} onChange={onChange} />
+    //   <input type="password" name='password' value={password} onChange={onChange} />
+    //   <input type="password" name='password2' value={password2} onChange={onChange} />
+    //   <button type='submit'>Register</button>
+    // </form>
   )
 }
 
