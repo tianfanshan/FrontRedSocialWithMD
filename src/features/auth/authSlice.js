d4abdd453f27ae37880a1a699f38645e843c846f
tianfanshan <stf30111994@gmail.com>
2022-07-01 09:54:59 +0200

    same action bug fixed

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -18,7 +18,7 @@ export const register = createAsyncThunk("auth/register", async (user) => {
   }
 });
 
-export const login = createAsyncThunk("auth/register", async (user) => {
+export const login = createAsyncThunk("auth/login", async (user) => {
   try {
     return await authService.login(user);
   } catch (error) {
