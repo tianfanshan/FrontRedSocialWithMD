43d98833046606f364156c616ee95771ad8eb039
tianfanshan <stf30111994@gmail.com>
2022-07-03 09:05:24 +0200

    port fixed

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000";
+const API_URL = "http://localhost:8080";
 
 const register = async (userData) => {
   console.log(userData)
--- src/features/posts/postsService.js
+++ src/features/posts/postsService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000";
+const API_URL = "http://localhost:8080";
 
 const getAllPost = async () => {
   const res = await axios.get(API_URL + "/posts");
