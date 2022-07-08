fa9f0c5ffec8121e08b5157fbaef11e4d470062d
tianfanshan <stf30111994@gmail.com>
2022-07-08 13:05:20 +0200

    start editmodal of post

--- src/components/Profile/EditModal/EditModal.jsx
+++ src/components/Profile/EditModal/EditModal.jsx
@@ -1,33 +1,49 @@
-import { useSelector } from "react-redux/es/exports"
-import { Button, Modal, Form, InputNumber, Select, Input } from "antd/lib/radio"
+import { useSelector, useDispatch } from "react-redux/es/exports"
+import { Button, Modal, Form, InputNumber, Select, Input } from "antd"
+import { useEffect } from "react"
+import { updatePost } from "../../../features/posts/postsSlice"
 
 const EditModal = ({visible,setVisible}) => {
 
+  const dispatch = useDispatch()
   // const { Option } = Select;
   const { post } = useSelector((state)=>state.posts)
   console.log(post)
 
+  const [form] = Form.useForm()
+
+  useEffect(()=>{
+    form.setFiledsValue()
+  },[])
+
+
+
   const onFinish = (values)=>{
-    console.log(values)
+    const postWithId = {...values,_id:post._id}
+    dispatch(updatePost(postWithId))
     setVisible(false)
   }
 
   return (
-    <h1>Holi</h1>
-    // <Modal
-    // title="Edit Post"
-    // visible={visible}
-    // footer={[]}
-    // >
-    //   <Form onFinish={onFinish}>
-    //     <Form.Item label="Post Name" name="name">
-    //       <input placeholder="Post name" />
-    //     </Form.Item>
-    //     <Form.Item>
-
-    //     </Form.Item>
-    //   </Form>
-    // </Modal>
+    <Modal
+    title="Edit Post"
+    visible={visible}
+    footer={[]}
+    >
+      <Form onFinish={onFinish}>
+        <Form.Item label="Post title" name="title">
+          <input placeholder="Post title" />
+        </Form.Item>
+        <Form.Item label="Post userName" name="userName">
+          <input placeholder="Post body" />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Click para ejecutar movida!
+          </Button>
+        </Form.Item>
+      </Form>
+    </Modal>
   )
 }
 
