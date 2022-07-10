d011e2034fafc1fbbf0e32286972f86d87d8d68e
tianfanshan <stf30111994@gmail.com>
2022-07-10 11:49:56 +0200

    bugs waiting to fix

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -19,6 +19,8 @@ const Profile = () => {
   const { posts } = useSelector((state) => state.posts)
   const { user } = useSelector((state) => state.auth)
 
+  console.log(user)
+
   const dispatch = useDispatch()
 
   const [isModalVisible, setIsModalVisible] = useState(false);
@@ -106,7 +108,64 @@ const Profile = () => {
     )
   })
 
+  const followers = user?.user.followers.map((f) => {
+    return (
+      <div>
+        <span>Nombre: {f.name}</span><br />
+        <span>Role: {f.role}</span>
+      </div>
+    )
+  })
 
+  const followings = user?.user.followings.map((f) => {
+    return (
+      <div>
+        <span>{f.name}</span>
+      </div>
+    )
+  })
+
+  const favorites = user?.user.favorites.map((pos) => {
+    const img = pos.images.map((im, i) => {
+      return (
+        <img alt="post-img" src={"http://localhost:8080/posts-images/" + im} key={i} />
+      )
+    })
+    const isAlreadyLiked = pos.likes?.includes(user?.user._id)
+    return (
+      <div key={pos._id}>
+        <div>
+          {pos.images.length > 0 ?
+            <Card
+              hoverable
+              style={{
+                width: 240,
+              }}
+              cover={img}
+            >
+              <Meta title={pos.userName} description={pos.body} />
+            </Card>
+            :
+            <Card
+              hoverable
+              style={{
+                width: 240,
+              }}
+            >
+              <Meta title={pos.userName} description={pos.body} />
+            </Card>
+          }
+          <>
+            <Button type="primary" onClick={() => showModal(pos._id)}>
+              Comentarios
+            </Button>
+          </>
+          <span className="wish">Likes: {pos.likes?.length}</span>
+          <HeartFilled />
+        </div>
+      </div>
+    )
+  })
 
   return (
     <div>
@@ -121,57 +180,57 @@ const Profile = () => {
         <Divider orientation="left" plain>
           Cuantos Años tienes!!!!!!🧓
         </Divider>
-        <span>{info.age}</span><br />
+        <span>{info?.age}</span><br />
         <Divider orientation="left" plain>
           Cuantas movidas has dejado en los posts😀
         </Divider>
-        <span>{info.commentId.length}</span><br />
+        <span>{info?.commentId.length}</span><br />
         <Divider orientation="left" plain>
           Y les gustan tus comentarios❗❓
         </Divider>
-        <span>{info.commentsLikes.length}</span><br />
+        <span>{info?.commentsLikes.length}</span><br />
         <Divider orientation="left" plain>
           Los posts que te has gustado...no me lo puedo creer🤡
         </Divider>
-        <span>{info.favorites.length}</span><br />
-        {info.followers.length < 1 ?
+        <span>{favorites}</span><br />
+        {info?.followers.length < 1 ?
           <div>
             <Divider orientation="left" plain>
               Nadie te quiere.../(ㄒoㄒ)/~~
             </Divider>
-            <span>Mis followers {info.followers.length}!!!</span><br />
+            <span>00000000000000000000</span><br />
           </div>
           :
           <div>
             <Divider orientation="left" plain>
               ┗|｀O′|┛ Alquien te esta persiguiendo ε=ε=ε=(~￣▽￣)~
             </Divider>
-            <span>{info.followers.length}</span><br />
+            <span>{followers}</span><br />
           </div>
         }
-        {info.followings.length < 1 ?
+        {info?.followings.length < 1 ?
           <div>
             <Divider orientation="left" plain>
               Muy chulo crack,quiere estar solo toda la vida ヾ(≧ ▽ ≦)ゝ
             </Divider>
-            <span>Quieres movida con {info.followings.length} usuarios</span><br />
+            <span>零零零零零零零零零零零零零零零零零零零</span><br />
           </div>
           :
           <div>
             <Divider orientation="left" plain>
               El boton esta fallando, no puede dar follower al usuario (╯▔皿▔)╯
             </Divider>
-            <span>Estas persiguiendo a {info.followings.length} humanos, o no...</span><br />
+            <span>Estas persiguiendo a {followings} humanos, o no...</span><br />
           </div>
         }
         <Divider orientation="left" plain>
           Tu nombre es👇
         </Divider>
-        <span>Es {info.name}!!!</span><br />
+        <span>Es {info?.name}!!!</span><br />
         <Divider orientation="left" plain>
           Quien eres👇
         </Divider>
-        <span>Soy {info.role}!!!</span><br />
+        <span>Soy {info?.role}!!!</span><br />
         <Divider orientation="left" plain>
           👇Posts!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!👇
         </Divider>
